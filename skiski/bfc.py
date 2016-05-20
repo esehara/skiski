2218c9ae153bbe722ad231a1cfc87a707a168178
esehara shigeo <esehara@gmail.com>
2016-05-21 03:02:25 +0900

    Refactor

--- skiski/bfc.py
+++ skiski/bfc.py
@@ -5,18 +5,18 @@ class B(metaclass=Typename("B")):
     """
     which composes two function
 
-    >>> B(lambda x: x).dot(lambda x: x + 5).dot(5).b()
+    >>> B(lambda x: x).dot(lambda x: x + 5).dot(5).w()
     10
-    >>> S(B).dot(I).dot(V("Succ")).b().b().dot("Zero").b()
+    >>> S(B).dot(I).dot(V("Succ")).w().w().dot("Zero").w()
     (Succ (Succ Zero))
     """
-    is_class = True
+
     def __init__(self, x):
         self.x = x
         self.is_class = False
 
     def dot(self, y):
-        return B2(self.x, y)
+        return _B2(self.x, y)
 
     def __str__(self):
         return "(B " + str(self.x) + ") "
@@ -24,15 +24,20 @@ class B(metaclass=Typename("B")):
     def __repr__(self):
         return self.__str__()
 
-    def b(self):
+    def w(self):
         return self
 
     @classmethod
     def to_ski(cls):
+        """
+        S(KS)K Combinator is virtually B Combinator:
+
+        >>>
+        """
         return S(K(S)).dot(K)
 
 
-class B2(metaclass=Typename("B")):
+class _B2(metaclass=Typename("B")):
 
     def __init__(self, x, y):
         self.x = x
@@ -40,12 +45,12 @@ class B2(metaclass=Typename("B")):
         self.is_class = False
 
     def dot(self, z):
-        return B3(self.x, self.y, z)
+        return _B3(self.x, self.y, z)
 
     def __str__(self):
         return "(B " + str(self.x) + " " + str(self.y) + ") "
 
-    def b(self):
+    def w(self):
         if not isinstance(self.y, type):
             self.y = self.y.b()
         return self
@@ -54,7 +59,7 @@ class B2(metaclass=Typename("B")):
         return "<" + self.__str__() + ">"
 
 
-class B3(metaclass=Typename("B")):
+class _B3(metaclass=Typename("B")):
 
     def __init__(self, x, y, z):
         self.x = x
@@ -62,12 +67,12 @@ class B3(metaclass=Typename("B")):
         self.z = z
         self.is_class = False
 
-    def b(self):
-        yz = self.__b__(self.y, self.z)
-        xyz = self.__b__(self.x, yz)
+    def w(self):
+        yz = self.__w__(self.y, self.z)
+        xyz = self.__w__(self.x, yz)
         return xyz
 
-    def __b__(self, x, y):
+    def __w__(self, x, y):
         if isinstance(x, type) or not hasattr(x, "__b__"):
             return x(y)
         else:
