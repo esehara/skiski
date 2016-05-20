4ba8e08f3c52c2b74444e1e2772bfe1355ee8db5
esehara shigeo <esehara@gmail.com>
2016-05-21 02:24:25 +0900

    add pytest

--- tests/ski_test.py
+++ tests/ski_test.py
@@ -1,2 +1,22 @@
-if __name__ == '__main__':
-    pass
+import pytest
+from skiski.ski import S, K, I, V
+
+
+def test_identity_operator():
+    assert I(1).w() == 1
+
+
+def test_identity_skk():
+    skk = S(K).dot(K).dot(1)
+    i = I(1)
+    assert skk.w().w() == i.w()
+
+
+def test_const_function():
+    assert K(10).dot(20).w() == 10
+
+
+def test_share_function():
+    a = lambda x: x * 5
+    b = lambda x: x - 3
+    assert S(a).dot(b).dot(5).w() == 10
