7f0dae30d7d554bf096419096f9542bfea4aa64d
esehara shigeo <esehara@gmail.com>
2016-05-21 03:36:56 +0900

    add B, F, C Combinator test

---
+++ tests/bfc_test.py
@@ -0,0 +1,16 @@
+import pytest
+from skiski.bfc import B
+
+
+def test_composite_function():
+    a = lambda x: x * 5
+    b = lambda x: x - 3
+    assert B(a).dot(b).dot(5).w() == 10
+
+
+def test_sksk_is_b():
+    a = lambda x: x * 5
+    b = lambda x: x - 3
+    b_comb = B(a).dot(b).dot(5).w()
+    sksk = B.to_ski().dot(a).w().dot(b).dot(5).w()
+    assert b_comb == sksk
