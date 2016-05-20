4121656b6d9bc91e1d8b824f335450957b4b0e4b
esehara shigeo <esehara@gmail.com>
2016-05-21 02:43:58 +0900

    prepare release

---
+++ setup.py
@@ -0,0 +1,19 @@
+from __future__ import with_statement
+from setuptools import setup
+import sys
+
+
+setup(
+    name="skiski",
+    version="0.1.0",
+    description="SKI Combinator for Python",
+    long_description="SKI Combinator for Python",
+    author="esehara shigeo",
+    author_email="esehara@gmail.com",
+    url="https://github.com/esehara/skiski",
+    py_modules=["skiski"],
+    include_package_data=True,
+    license="MIT",
+    zip_safe=False,
+    classifiers=[]
+)
